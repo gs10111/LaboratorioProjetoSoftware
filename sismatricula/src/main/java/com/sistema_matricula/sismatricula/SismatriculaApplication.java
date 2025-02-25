3d44e53246fd6240a50cf12eb3fb063aaa33f431
KaylerMm <kaylemoura.km@gmail.com>
2025-02-25 16:47:51 -0300

    feat: Initializing SpringBoot project

---
+++ sismatricula/src/main/java/com/sistema_matricula/sismatricula/SismatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_matricula.sismatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismatriculaApplication.class, args);
+	}
+
+}
---
+++ sismatricula/src/test/java/com/sistema_matricula/sismatricula/SismatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_matricula.sismatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
