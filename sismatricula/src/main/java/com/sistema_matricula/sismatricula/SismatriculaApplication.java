13f95f6aab6a73243709a304741748d8a8278d84
KaylerMm <kaylemoura.km@gmail.com>
2025-03-09 20:23:20 -0300

    feat: Adding mock startup for testing application

--- sismatricula/src/main/java/com/sistema_matricula/sismatricula/SismatriculaApplication.java
+++ sismatricula/src/main/java/com/sistema_matricula/sismatricula/SismatriculaApplication.java
@@ -1,13 +1,18 @@
 package com.sistema_matricula.sismatricula;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SismatriculaApplication {
+public class SismatriculaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SismatriculaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SismatriculaApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        MainMock.main(args);
+    }
 }
